489e1193f9ad4cc9d4c6281e239baa90db5dcdb8
Diego Alejandro Becerra Lopez <diego.becerra1994@gmail.com>
2023-02-23 10:27:22 -0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ mkdir data
 mkdir static/tmp
 docker build -f Dockerfile -t automatizationpdf:latest .
 REM docker run --name db -d --env-file .env -v "%cd%/data:/var/lib/postgresql/data" -p 5432:5432 postgres:14.1-alpine
-sleep 20
+REM sleep 20
 docker run -it --rm -w /usr/src/app/ -p 80:8080 automatizationpdf:latest
